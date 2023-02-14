b143b899764731571f49b0bbdd15696646a72411
Cushione <maulerovak@gmail.com>
2023-02-14 16:24:59 +0000

    Add Profile model

--- photora_api/settings.py
+++ photora_api/settings.py
@@ -151,7 +151,7 @@ CLOUDINARY_STORAGE = {
 }
 
 MEDIA_URL = '/media/'
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinary'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
