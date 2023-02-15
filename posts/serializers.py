059ccea9b804476e9d178d5c543b7a24a7f2411b
Cushione <maulerovak@gmail.com>
2023-02-15 17:56:12 +0000

    Add create post view and image validation

--- posts/serializers.py
+++ posts/serializers.py
@@ -9,6 +9,22 @@ class PostSerializer(serializers.ModelSerializer):
     def get_is_owner(self, obj):
         return self.context['request'].user == obj.owner
 
+
+    def validate_image(self, value):
+        if value.size > 1024 * 1024 * 2:
+            raise serializers.ValidationError(
+                'Image size is larger than 2MB!'
+            )
+        if value.image.height > 4096:
+            raise serializers.ValidationError(
+                'Image height is larger than 4096px!'
+            )
+        if value.image.width > 4096:
+            raise serializers.ValidationError(
+                'Image width is larger than 4096px!'
+            )
+        return value
+
     class Meta:
         model = Post
         fields = [
