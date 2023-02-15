29d408905db9b874bef1557640d413c0435b1a64
Cushione <maulerovak@gmail.com>
2023-02-15 17:03:23 +0000

    Add post app and model

--- photora_api/settings.py
+++ photora_api/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'corsheaders',
     'profiles',
+    'posts',
 ]
 
 MIDDLEWARE = [
---
+++ posts/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Post(models.Model):
+    owner = models.OneToOneField(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    image = models.ImageField(
+        upload_to='images/' 
+    )
+    likes = models.ManyToManyField(
+        User, related_name='post_likes', blank=True
+    )
+
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.title
+
+    
