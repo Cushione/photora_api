877e9e43acc1e610ff818f852ccffde2f83efbab
Cushione <maulerovak@gmail.com>
2023-03-11 11:03:36 +0000

    Add is owner and profile name instead of username to profile list

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -25,14 +25,18 @@ class ProfileSerializer(serializers.ModelSerializer):
 class ProfileListSerializer(serializers.ModelSerializer):
     profile_id = serializers.ReadOnlyField(source='profile.id')
     profile_image = serializers.ReadOnlyField(source='profile.image.url')
+    profile_name = serializers.ReadOnlyField(source='profile.name')
+    is_owner = serializers.SerializerMethodField()
     is_followed = serializers.SerializerMethodField()
 
     def get_is_followed(self, obj):
         return obj.profile.followers.filter(id=self.context['request'].user.id).exists()
+        
+    def get_is_owner(self, obj):
+        return self.context['request'].user == obj
 
     class Meta:
         model = User
-        read_only_fields = ['username']
         fields = [
-            'profile_id', 'profile_image', 'is_followed'
+            'profile_id', 'profile_image', 'profile_name', 'is_followed', 'is_owner'
         ]
