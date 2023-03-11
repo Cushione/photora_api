114962317bbd575b6f2109eda599929c81f57bc0
Cushione <maulerovak@gmail.com>
2023-03-11 11:08:05 +0000

    Make comment content mandatory

--- comments/models.py
+++ comments/models.py
@@ -7,7 +7,7 @@ class Comment(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    content = models.TextField(blank=True)
+    content = models.TextField()
     post = models.ForeignKey(
         Post, on_delete=models.CASCADE, related_name="post_comments"
         )
