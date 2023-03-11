903e26c8d6e384315604143b35915dd6b33f77d5
Cushione <maulerovak@gmail.com>
2023-03-11 11:08:35 +0000

    Replace owner with profile name in comment serializer

--- comments/serializers.py
+++ comments/serializers.py
@@ -3,10 +3,10 @@ from .models import Comment
 
 
 class CommentSerializer(serializers.ModelSerializer):
-    owner = serializers.ReadOnlyField(source='owner.username')
     post = serializers.ReadOnlyField(source='post.id')
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+    profile_name = serializers.ReadOnlyField(source='owner.profile.name')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
 
     def get_is_owner(self, obj):
@@ -15,6 +15,6 @@ class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
         fields = [
-            'id', 'owner', 'post', 'created_at', 'content',
+            'id', 'post', 'created_at', 'content', 'profile_name',
             'is_owner', 'profile_id', 'profile_image'
         ]
