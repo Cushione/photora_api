9a0347857576f22c3a2e23a0e1f3716677782f1e
Kristyna Wach <113436209+Cushione@users.noreply.github.com>
2024-11-12 16:51:11 +0100

    Move allowed hosts settings to environment variable

--- photora_api/settings.py
+++ photora_api/settings.py
@@ -30,12 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = "ENV" in os.environ and os.environ["ENV"] == "DEV"
 
-ALLOWED_HOSTS = [
-    "photora.herokuapp.com",
-    "photora-api.herokuapp.com",
-    "localhost",
-]
-
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(",")
 
 # Application definition
 
